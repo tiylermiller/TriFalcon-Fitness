581f8705295ce17910a10cc4cff11471eb9c2d17
alexi <alexi@A-LAPTOP>
2026-02-08 21:41:31 -0500

    Add project files.

---
+++ TheLootList/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TriFalcon_Fitness.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ TheLootList/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TriFalcon_Fitness.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
